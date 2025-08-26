8ae115a7b20e40f421958b1978ce4b2b95df67c7
jekisaputra40 <98233323+jekisaputra40@users.noreply.github.com>
2025-08-26 15:01:59 +0000

    dfuyhsjhbughwetreyreyrfghjh

---
+++ admin/dashboard.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Dashboard</title>
+</head>
+<body style="background:black; color:lime; font-family:monospace; text-align:center; padding-top:50px;">
+  <h1>[+] Selamat datang di halaman rahasia!</h1>
+  <p>Login sukses → sekarang kamu seolah sudah masuk sistem 👻</p>
+</body>
+</html>
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Dashboard</title>
+</head>
+<body style="background:black; color:lime; font-family:monospace; text-align:center; padding-top:50px;">
+  <h1>[+] Selamat datang di halaman rahasia!</h1>
+  <p>Login sukses → sekarang kamu seolah sudah masuk sistem 👻</p>
+</body>
+</html>
+
+<!--
+    <h2>Login</h2>
+    
+    <form method="POST" action="">
+      <input type="text" name="username" placeholder="Username" required><br>
+      <input type="password" name="password" placeholder="Password" required><br>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+
