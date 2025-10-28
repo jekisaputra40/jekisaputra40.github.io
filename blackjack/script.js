5bd2380f0123a548bad1f1211c42739866b0103a
jekisaputra40 <98233323+jekisaputra40@users.noreply.github.com>
2025-10-28 13:54:41 +0000

    dfhsdfhsjdhf

---
+++ blackjack/script.js
@@ -0,0 +1,11 @@
+// Tambah animasi saat scroll
+const fadeEls = document.querySelectorAll('.fade-in');
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
+
+fadeEls.forEach(el => observer.observe(el));
